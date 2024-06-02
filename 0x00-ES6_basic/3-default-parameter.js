672cb4c14b7d53b8f2c3b03b5f034e91aed59503
realFrankieWilson <frankuwill101@gmail.com>
2024-06-02 15:51:57 +0100

    modified file 3-default

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,4 +1,8 @@
 export default function getSumOfHoods(initialNumber, expansion1989, expansion2019) {
+  // return (expansion1989 === undefined ? 89 : expansion1989)
+  //   + (expansion2019 === undefined ? 19 : expansion1989) + initialNumber;
+
   return (expansion1989 === undefined ? 89 : expansion1989)
-    + (expansion2019 === undefined ? 19 : expansion1989) + initialNumber;
+    + (expansion2019 === undefined ? 19 : expansion2019)
+    + initialNumber;
 }
