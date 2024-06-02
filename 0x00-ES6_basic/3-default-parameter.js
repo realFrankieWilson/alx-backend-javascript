7825d35a2483cdd53e7a782cc9e51dab8bb1df65
realFrankieWilson <frankuwill101@gmail.com>
2024-06-02 15:55:14 +0100

    a quick fix for file 3-deafult

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -2,7 +2,6 @@ export default function getSumOfHoods(initialNumber, expansion1989, expansion201
   // return (expansion1989 === undefined ? 89 : expansion1989)
   //   + (expansion2019 === undefined ? 19 : expansion1989) + initialNumber;
 
-  return (expansion1989 === undefined ? 89 : expansion1989)
-    + (expansion2019 === undefined ? 19 : expansion2019)
-    + initialNumber;
+  return ((expansion1989 === undefined ? 89 : expansion1989)
+    + (expansion2019 === undefined ? 19 : expansion2019) + initialNumber);
 }
