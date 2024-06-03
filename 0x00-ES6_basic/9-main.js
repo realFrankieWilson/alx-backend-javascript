7ac131736ce78f21a26df098724326d4a710438d
realFrankieWilson <frankuwill101@gmail.com>
2024-06-03 07:23:09 +0100

    added more tasks

---
+++ 0x00-ES6_basic/9-main.js
@@ -0,0 +1,7 @@
+import getFullBudgetObject from "./9-getFullBudget";
+
+const fullBudget = getFullBudgetObject(20, 50, 10);
+
+console.log(fullBudget.getIncomeInDollars(fullBudget.income));
+
+console.log(fullBudget.gdtIncomeInEuros(fullBudget.income));
