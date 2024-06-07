9d5d48f185030eec7839d5c65f8eb37e9ee60996
realFrankieWilson <frankuwill101@gmail.com>
2024-06-07 17:12:24 +0100

    hot fix

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,7 +1,10 @@
-/* eslint-disable no-underscore-dangle */
+// Represents a classroom with a maximum of student capacity
 export default class ClassRoom {
+  /**
+   * Creates an instance of ClassRoom
+   * @param {maxStudentSize} maxStudentSize
+   */
   constructor(maxStudentSize) {
-    this._maxStudentSize = Number(maxStudentSize);
+    this._maxStudentSize = maxStudentSize;
   }
 }
-/* eslint-enable no-underscore-dangle */
