cb9f1ba1305ce42f33800ee2b296c2f17970cb3d
realFrankieWilson <frankuwill101@gmail.com>
2024-06-09 18:14:16 +0100

    task 2 done

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,16 @@
+/**
+ * A function that filters student's list based on student's location.
+ * @param {Array<string and number>} arrayObj - The array of lists containing students.
+ * @param {string} city - The location of the student.
+ *
+ * @returns {Array<string>} - An array of objects of students who are
+ * located in a specific city.
+*/
+
+export default function getStudentsByLocation(arrayObj, city) {
+  if (Array.isArray(arrayObj)) {
+    return arrayObj.filter((obj) => obj.location === city);
+  }
+
+  return [];
+}
