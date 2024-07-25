a3df02e2de9aa96a0c0577c28905a63a885ff15e
realFrankieWilson <frankuwill101@gmail.com>
2024-07-25 13:51:53 +0100

    hot fix on file 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,12 +4,12 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 if (process.stdin.isTTY) {
   process.stdin.on('data', (data) => {
     process.stdout.write(`Your name is: ${data.toString()}`);
-    process.exit(0);
+    process.exit();
   });
 } else {
   process.stdin.on('data', (data) => {
     process.stdout.write(`Your name is: ${data.toString()}`);
-    process.exit(0);
+    process.exit();
   });
   process.on('exit', () => {
     process.stdout.write('This important software is now closing\n');
