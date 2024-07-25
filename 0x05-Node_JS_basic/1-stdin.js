29b87f3c3ee1b05abd18eb9cda588068125b6a8c
realFrankieWilson <frankuwill101@gmail.com>
2024-07-25 13:43:41 +0100

    hot fix

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,26 +1,18 @@
 // 1-stdin.js
 
-const readline = require('readline');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Create an interface for input and output
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-// Welcome message
-console.log('Welcome to Holberton School, what is your name?');
-
-// Prompt the user for their name
-rl.on('line', (input) => {
-  console.log(`Your name is: ${input}`);
-
-  // Automatically close the interface after displaying the name
-  rl.close();
-});
-
-// Handle the close event
-rl.on('close', () => {
-  console.log('This important software is now clossing');
-  process.exit(0);
-});
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit(0);
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit(0);
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
