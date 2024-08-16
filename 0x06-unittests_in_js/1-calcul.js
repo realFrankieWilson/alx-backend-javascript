3dd6d8bdda9cf760679660ba7582c3113e277475
realfrankiewilson <frankuwill101@gmail.com>
2024-08-16 09:44:43 -0700

    added task 1

---
+++ 0x06-unittests_in_js/1-calcul.js
@@ -0,0 +1,21 @@
+// ./0-calcul.js
+function calculateNumber(type, a, b) {
+  const A = Math.round(a);
+  const B = Math.round(b);
+
+  switch (type) {
+    case "SUM":
+      return A + B;
+    case "SUBTRACT":
+      return A - B;
+    case "DIVIDE":
+      if (B === 0){
+        return 'Error'
+      }
+      return A / B;
+    default:
+      throw new Error('Invalid operation type');
+  }
+}
+
+module.exports = calculateNumber;
