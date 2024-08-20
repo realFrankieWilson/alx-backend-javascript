37f102959ea372bc03d700fec4e0e819d9881942
Frankie wilson <132236591+realFrankieWilson@users.noreply.github.com>
2024-08-20 03:19:10 -0700

    Delete 0x05-Node_JS_basics directory
    diff in timezones

--- 0x05-Node_JS_basics/full_server/controllers/AppController.js
+++
@@ -1,6 +0,0 @@
-// full_server/controllers/AppController.js
-export default class AppController {
-  static getHomepage(req, res) {
-    return res.status(200).send('Hello Holberton School!');
-  }
-}
