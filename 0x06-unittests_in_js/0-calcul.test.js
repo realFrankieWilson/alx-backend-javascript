3d87404c987340663574496869f11442ff18bd23
realfrankiewilson <frankuwill101@gmail.com>
2024-08-23 06:21:51 +0000

    hot fix

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,27 +2,27 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
-  it('should return the sum of rounded number', () => {
-    assert.equal(calculateNumber(2.5, 2.5), 6);
+  it('should return 4 for inpuuts 2.4, and 2.3', () => {
+    assert.strictEqual(calculateNumber(2.4, 2.3), 4);
   });
-  it('should return the sum when both numbers are negative', () => {
-    assert.equal(calculateNumber(-2.5, -3.5), -5);
+  
+  it('should return 6 for inputs 2.5, and 2.5', () => {
+    assert.strictEqual(calculateNumber(2.5, 2.5), 6);
   });
-  it('should return the correct sum when one number is negative', () => {
-    assert.equal(calculateNumber(-2.5, 3.5), 2);
+
+  it('should return 0 for inputs 0.4 and -0.4', () => {
+    assert.strictEqual(calculateNumber(0.4, -0.4), 0);
+  })
+
+  it('should return -0 for inputs 0-.5 and -0.5', () => {
+    assert.strictEqual(calculateNumber(-0.5, -0.5), -0);
   });
-  it('shouldd return the sum when both numbers are positive integers', () => {
-    assert.equal(calculateNumber(2, 3), 5);
+
+  it('should return 1 for inputs 0.5 and 0.4', () => {
+    assert.strictEqual(calculateNumber(0.5, 0.4), 1);
   });
-  it('should return the sum when one number is zero', () => {
-    assert.equal(calculateNumber(0, 2.5), 3);
-    assert.equal(calculateNumber(4.5, 0), 5);
-  });
-  it('should return 0 when both numbers are zero', () => {
-    assert.equal(calculateNumber(0, 0), 0);
-  });
-  it('should correctly round numbers', () => {
-    assert.equal(calculateNumber(2.4, 2.9), 5);
-    assert.equal(calculateNumber(-2.5, -11.1), -13);
-  });
+
+  it('should return 4 for inputs 3.9 and 0.1', () => {
+    assert.strictEqual(calculateNumber(3.9, 0.1), 4);
+  })
 })
